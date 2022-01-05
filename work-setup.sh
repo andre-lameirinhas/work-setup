e55bc06412ca26301ef2b33176530fb9c67bc50d
André Lameirinhas <andre.lameirinhas@gmail.com>
2022-01-05 19:22:06 +0000

    Add brew, iterm2 and virtualenv

--- work-setup.sh
+++ work-setup.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
 
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-# plugins = git docker web-search
-
+# brew
+xcode-select --install
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew update
 
+# ohmyzsh (plugins = git docker web-search)
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# fzf
 brew install fzf
 $(brew --prefix)/opt/fzf/install
 
+# python stuff
 brew install pyenv
 pyenv install 3.10.0
 pyenv shell 3.10.0
+sudo pip install virtualenv
+sudo pip install virtualenvwrapper
 
+# z
 curl -o ~/z.sh https://raw.githubusercontent.com/rupa/z/master/z.sh
 echo '. ~/z.sh' >> ~/.zshrc
 
+# bat
 brew install bat
 echo 'alias cat=bat' >> ~/.zshrc
 
 source ~/.zshrc
 
-brew install --cask visual-studio-code docker
+# casks
+brew install --cask iterm2 visual-studio-code docker
