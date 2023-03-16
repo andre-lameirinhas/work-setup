b8c2a18edb022f9a8a310b1cab1c9010feabc9f4
André Lameirinhas <andre@andres-mbp.lan>
2023-03-16 03:05:17 +0000

    Add .zshrc and remove writing into .zshrc

---
+++ .zshrc
@@ -0,0 +1,31 @@
+# oh-my-zsh
+export ZSH="$HOME/.oh-my-zsh"
+ZSH_THEME="robbyrussell"
+plugins=(git docker docker-compose)
+source $ZSH/oh-my-zsh.sh
+
+# homebrew
+eval "$(/usr/local/bin/brew shellenv)"
+
+# starship
+eval "$(starship init zsh)"
+
+# fzf
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
+# z
+. ~/z.sh
+
+# pyenv
+export PYENV_ROOT="$HOME/.pyenv"
+command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+
+# pyenv-virtualenvwrapper
+export PYENV_VIRTUALENVWRAPPER_PREFER_PYENV="true"
+export WORKON_HOME=$HOME/.virtualenvs
+pyenv virtualenvwrapper_lazy
+
+# alias
+alias cat=bat
+alias vim=nvim
--- work-setup.sh
+++ work-setup.sh
@@ -2,58 +2,49 @@
 set -euo pipefail
 
 # oh-my-zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-echo 'plugins=(git docker docker-compose)' >> ~/.zshrc
+# add if
+#sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # brew
-xcode-select --install
+# add if
+#xcode-select --install
+# add if
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-echo 'eval "$(/usr/local/bin/brew shellenv)"' >> ~/.zshrc
-source ~/.zshrc
-brew update
 
 # starship
 brew install starship
-echo 'eval "$(starship init zsh)"' >> ~/.zshrc
+brew tap homebrew/cask-fonts && brew install --cask font-fira-code-nerd-font
 
 # git
 brew install git git-gui
 
 # fzf
 brew install fzf
-$(brew --prefix)/opt/fzf/install
+$(brew --prefix)/opt/fzf/install --completion --no-update-rc
 
 # z
 curl -o ~/z.sh https://raw.githubusercontent.com/rupa/z/master/z.sh
-echo '. ~/z.sh' >> ~/.zshrc
 
 # bat
 brew install bat
-echo 'alias cat=bat' >> ~/.zshrc
 
 # nvim
 brew install neovim
-echo 'alias vim=nvim' >> ~/.zshrc
 
 # pyenv
 brew install pyenv
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
-echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
-echo 'eval "$(pyenv init -)"' >> ~/.zshrc
-source ~/.zshrc
-
-# python
-echo 'PATH=$(pyenv root)/shims:$PATH' >> ~/.zshrc
-pyenv install 3.10.0
-pyenv shell 3.10.0
-python -m pip install --upgrade pip
 
 # pyenv-virtualenvwrapper
 brew install pyenv-virtualenvwrapper
-echo 'export PYENV_VIRTUALENVWRAPPER_PREFER_PYENV="true"' >> ~/.zshrc
-echo 'export WORKON_HOME=$HOME/.virtualenvs' >> ~/.zshrc
-echo 'pyenv virtualenvwrapper_lazy' >> ~/.zshrc
-source ~/.zshrc
+
+# python
+brew install xz
+pyenv install 3.11.2
+pyenv global 3.11.2
+python -m pip install --upgrade pip setuptools
 
 # casks
 brew install --cask iterm2 visual-studio-code docker rectangle
+
+cp .zshrc ~/.zshrc
+source ~/.zshrc
