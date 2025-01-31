d93ce6f620a0425ab0cd3f73588eb8bfa3ac772a
André Lameirinhas <andre.lameirinhas@gmail.com>
2025-01-31 12:30:37 +0000

    Add delta line numbers and add spacing to coffee image

--- work-setup.sh
+++ work-setup.sh
@@ -49,6 +49,7 @@ git config --global rebase.updateRefs true
 git config --global core.pager delta
 git config --global interactive.diffFilter 'delta --color-only'
 git config --global delta.navigate true
+git config --global delta.line-numbers true
 git config --global merge.conflictStyle zdiff3
 git config --global diff.colorMoved default
 
